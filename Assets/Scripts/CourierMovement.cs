5327451ea78b15077daedafaada2a5be1e4a49f1
emrecang <emre1907can1907@gmail.com>
2019-07-11 23:01:56 +0300

    Unit and Courier Movement

---
+++ Assets/Scripts/CourierMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourierMovement : MonoBehaviour
+{
+    float maxDistance = 2000;
+    [SerializeField] float rotSpeed = 20f;
+    [SerializeField] float moveSpeed = 400f;
+    bool isMoving;
+    Vector3 targetPosition;
+    Vector3 lookAtTarget;
+    Quaternion playerRot;
+    
+    private void Update()
+    {
+        PlayerControllSettings();
+    }
+
+    void PlayerControllSettings()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            SetTargetPositionForMove();
+        }
+        if(isMoving)
+        {
+            Move();
+        }
+    }
+
+    void SetTargetPositionForMove()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, maxDistance))
+        {
+            targetPosition = hit.point;
+            lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
+                                       0, targetPosition.z - transform.position.z);
+            if(lookAtTarget != Vector3.zero)
+                playerRot = Quaternion.LookRotation(lookAtTarget);
+
+            isMoving = true;
+        }
+    }
+
+    private void Move()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                playerRot,
+                                                rotSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, 
+                                                new Vector3(targetPosition.x, transform.localScale.y/2 +1 ,targetPosition.z), 
+                                                moveSpeed * Time.deltaTime);
+
+        if(transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/UnitsMove.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class UnitsMove : MonoBehaviour
+{
+    float maxDistance = 5000f;
+    bool onMove = false;
+   [SerializeField] GameObject selectedUnit;
+
+    private void Update()
+    {
+        PlayerControllSettings();
+        SendRayToMousePosition();
+    }
+
+    public void SendRayToMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
+            SelectUnit(hit);
+        }
+    }
+
+    public void SelectUnit(RaycastHit hit)
+    {
+        if (hit.collider.gameObject.CompareTag("Unit") && onMove)
+        {
+            if (selectedUnit == null)
+            {
+                selectedUnit = hit.collider.gameObject;
+            }
+        }
+
+        if(onMove)
+            MoveUnit(hit);
+    }
+
+    public void MoveUnit(RaycastHit hit)
+    {
+        if (selectedUnit != null)
+        {
+            Vector3 mousePos = new Vector3(hit.point.x, (hit.transform.localScale.y / 2) + 3, hit.point.z);
+
+            while (selectedUnit.transform.position != mousePos)
+            { selectedUnit.transform.position = mousePos; }
+        }
+    }
+
+    public void PlayerControllSettings()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Q))
+        {
+            if (onMove)
+            {
+                onMove = false;
+                selectedUnit = null;
+            }
+            else
+                onMove = true;
+        }
+    }
+
+}
