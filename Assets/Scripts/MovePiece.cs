0c53e29af66423d63b73f24e5e056c2f0704c0ee
riwmd <kubilayege95@gmail.com>
2019-07-16 01:42:56 +0300

    finmp

--- Assets/Scripts/MovePiece.cs
+++ Assets/Scripts/MovePiece.cs
@@ -40,24 +40,27 @@ public class MovePiece : MonoBehaviour
 
     }
 
-    public RaycastHit SendRayToMousePosition()
+    public GameObject SendRayToMousePosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit, maxRayDistance))
         {
-            return hit;
+            return hit.collider.gameObject;
         }
-        return hit;
+        else
+        {
+            return null;
+        }
     }
 
     public void SelectUnit()
     {
-        RaycastHit unitCandidate = SendRayToMousePosition();
+        GameObject unitCandidate = SendRayToMousePosition();
         if (unitCandidate != null)
         {
-            if (unitCandidate.collider.gameObject.CompareTag("Unit"))
+            if (unitCandidate.CompareTag("Unit"))
             {
-                selectedUnit = unitCandidate.collider.gameObject;
+                selectedUnit = unitCandidate.gameObject;
                 originOfSelectedUnit = selectedUnit.transform;
                 candidateObj = GameObject.Instantiate(candidatePrefab, new Vector3(selectedUnit.transform.position.x, 1.0f, selectedUnit.transform.position.z), Quaternion.identity);
             }
@@ -66,39 +69,39 @@ public class MovePiece : MonoBehaviour
 
     public void MoveUnit(GameObject selectedUnit) //bu kod doğru...
     {
-        RaycastHit placeCandidate = SendRayToMousePosition();
-        if (placeCandidate.collider.gameObject.CompareTag("Unit"))
+        GameObject placeCandidate = SendRayToMousePosition();
+        if (placeCandidate != null && placeCandidate.CompareTag("Unit"))
         {
             Vector3 newPos = placeCandidate.transform.position;
-            placeCandidate.collider.gameObject.transform.parent.position = originOfSelectedUnit.position;
+            placeCandidate.transform.parent.position = originOfSelectedUnit.position;
             selectedUnit.transform.parent.position = newPos;
         }
         else
         {
             selectedUnit.transform.parent.position = placableBoardPosition(placeCandidate);
         }
+
         Destroy(candidateObj);
     }
     public void candidatePlaceHolder()
     {
         Vector3 tempPos = placableBoardPosition(SendRayToMousePosition());
+
         candidateObj.transform.position = new Vector3(tempPos.x, 1, tempPos.z);
     }
-    Vector3 placableBoardPosition(RaycastHit candidatePlace)
+    Vector3 placableBoardPosition(GameObject candidatePlace)
     {
         Vector3 newPos = originOfSelectedUnit.transform.position;
-        //if (candidatePlace.collider.gameObject.CompareTag("Unit"))
-        //{
-        //    newPos = candidatePlace.transform.position;
-        //    candidatePlace.collider.gameObject.transform.parent.position = originOfSelectedUnit.position;
-        //    return newPos;
-        //}
-        for (int i = 0; i < playerController.chessboardSize / 2; i++)
+
+        if(candidatePlace != null)
         {
-            if(candidatePlace.transform.gameObject.name == playerController.chessboardPieces[i].gameObject.name)
+            for (int i = 0; i < playerController.chessboardSize / 2; i++)
             {
-                newPos = new Vector3(candidatePlace.transform.position.x, (selectedUnit.transform.localScale.y / 2) + 3, candidatePlace.transform.position.z);
-                return newPos;
+                if (candidatePlace.transform.gameObject.name == playerController.chessboardPieces[i].gameObject.name)
+                {
+                    newPos = new Vector3(candidatePlace.transform.position.x, (selectedUnit.transform.localScale.y / 2) + 3, candidatePlace.transform.position.z);
+                    return newPos;
+                }
             }
         }
         return newPos;
