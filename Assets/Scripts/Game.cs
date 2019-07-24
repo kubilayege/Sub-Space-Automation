f0fc3d1614915aca5a49dcc2695ddc4974db92ad
riwmd <kubilayege95@gmail.com>
2019-07-25 02:38:27 +0300

    UIBasics

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject matchPrefab;
+
+    public void PlayGame()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        GameObject match = GameObject.Instantiate(matchPrefab, Vector3.zero, Quaternion.identity, this.transform);
+    }
+
+}
---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public GameObject shopPanel;
+
+    public void Start()
+    {
+        shopPanel = transform.GetChild(1).transform.GetChild(1).gameObject;
+    }
+
+
+    public void ToggleShop()
+    {
+        shopPanel.SetActive(!shopPanel.activeInHierarchy);
+    }
+}
