45bfe84405ccf29073f1cea40974072d63d925cb
riwmd <kubilayege95@gmail.com>
2019-07-25 03:53:09 +0300

    ShopUI

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public List<GameObject> piecesOnPool;
+    public List<Transform> pieceSlotsList;
+    public List<GameObject> tempShopPieces;
+
+    void Start()
+    {
+        InitializeVariables();
+        DrawPieces();
+    }
+    
+    void Update()
+    {
+       
+    }
+
+    void InitializeVariables()
+    {
+        GameObject pieceSlots = transform.GetChild(0).gameObject;
+        for(int i = 0; i< pieceSlots.transform.childCount ; i++)
+        {
+            pieceSlotsList.Add(pieceSlots.transform.GetChild(i));
+        }
+    }
+
+    void DrawPieces()
+    {
+        for(int i=0; i < 5; i++)
+        {
+            int maxIndex = piecesOnPool.Count - 1;
+            int randomIndex = Random.Range(0, maxIndex);
+            Debug.Log(piecesOnPool.Count);
+            tempShopPieces.Add(Instantiate(piecesOnPool[randomIndex], pieceSlotsList[i].transform.position, Quaternion.identity, pieceSlotsList[i].transform));
+            
+            piecesOnPool.RemoveAt(randomIndex);
+        }
+    }
+}
