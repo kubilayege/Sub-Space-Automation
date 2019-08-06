e152796e479946bfb91a5ae803d73d94ba86c449
riwmd <kubilayege95@gmail.com>
2019-08-06 18:43:07 +0300

    GoldIncome

--- Assets/Scripts/EventTest.cs
+++ Assets/Scripts/EventTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,23 +11,42 @@ public class EventTest : MonoBehaviour
     [SerializeField] int round = 0;
     [SerializeField] int preaperOrFight = 1; //1 = preaper, -1 = fight;
     Shop shop;
+    [SerializeField] List<PlayerPurse> purses;
     void Start()
     {
-
-        shop = transform.parent.GetChild(1).GetChild(0).GetComponent<Shop>();
+        GetPurses();
+        shop = transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Shop>();
         PreaperingRound(preaperingTime);
-
+        
     }
+
+    private void GetPurses()
+    {
+        for (int i=1; i < 9; i++)
+        {
+            purses.Add(transform.GetChild(i).GetChild(3).GetComponent<PlayerPurse>());
+        }
+    }
+
+
 
     void PreaperingRound(float time)
     {
-        
+        GenerateIncome();
         StartCoroutine(DecreaseTime(time));
         //unlockMoveBoard();
         ++round;
         shop.ClearSlot();
         shop.DrawPieces();
         
+    }
+
+    private void GenerateIncome()
+    {
+        for (int i = 0; i < purses.Count; i++)
+        {
+            purses[i].CalculateIncome(round, true); // bool parametresi matchupların sonucuna göre gönderilecek.
+        }
     }
 
     void FightRound(float time)
