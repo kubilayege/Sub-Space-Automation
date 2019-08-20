a6149331b8250413f23164a1a58da23a8cf47b5f
emrecang <emre1907can1907@gmail.com>
2019-08-20 04:27:28 +0300

    unit upgrade

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -7,6 +7,8 @@ public class Shop : MonoBehaviour
     public List<GameObject> gamePieces; // Oyundaki her piece burda tutulacak
     public List<Transform> pieceSlotsList; //Shop slot pozisyonları listesi
     public List<GameObject> tempShopPieces;
+    public List<GameObject> tempUpgradeUnit; //upgrade için
+    public int starCount = 0;
     float maxRayDistance = 5000;
     int poolSize = 100;
     public Match match;
@@ -30,7 +32,6 @@ public class Shop : MonoBehaviour
         {
             pieceSlotsList.Add(pieceSlots.transform.GetChild(i));
         }
-
         match = transform.parent.parent.parent.parent.GetComponent<Match>();
         board = match.boards[0].GetComponent<Board>();
     }
@@ -100,7 +101,7 @@ public class Shop : MonoBehaviour
             }
 
             purse.ModifyGold(-(tempSelectedUnit.transform.parent.GetComponent<Pieces>().pieceCost));
-
+           
             for (int i = 0; i < board.benchSize; i++)
             {
                 if (board.playerBenchList[i] == null)
@@ -111,14 +112,100 @@ public class Shop : MonoBehaviour
                     board.playerBenchList[i] = tempSelectedUnit.transform.parent.gameObject;
                     tempShopPieces.Remove(tempSelectedUnit.transform.parent.gameObject);  //Shop ekranındaki gösterilen listeden siliniyor
                     tempSelectedUnit.transform.parent.parent = board.benchPosition[i].transform; //Bench blokğunun child'ı oluyor
+
+                    ///*eğer satın alınan nesne ile nesnenin piece scriptindeki adı aynı değilse örneğin p_examplemodel_1 != warwick*
+                    // bu durumda bu karakterden daha önce alınmamış demektir. bu durumda o karakterin isminde bir yuva açıyoruz.
+                    // */
+                    //if (tempSelectedUnit.transform.parent.name != tempSelectedUnit.transform.parent.GetComponent<Pieces>().pieceName)
+                    //{
+                    //    GameObject emptyGameObject = new GameObject();
+                    //    emptyGameObject.name = tempSelectedUnit.transform.parent.GetComponent<Pieces>().pieceName;
+                    //    emptyGameObject.transform.parent = board.benchPosition[i].transform;
+                    //    tempSelectedUnit.transform.parent.parent = emptyGameObject.transform;
+                    //}
+                    //else
+                    //{
+                    //}
+                    UpgradeUnit(tempSelectedUnit);
                     tempSelectedUnit = null;
                     break;
 
                 }
+                
+            }
+            
+        }
+        
+
+    }
+    public void UpgradeUnit(GameObject unit)
+    {
+        
+        for (int i = 0; i < 32; i++)
+        {
+            Debug.Log("Aşama 1");
+            if (board.chessboardPosition[i].transform.childCount > 0)
+            {
+                Debug.Log("Aşama 2");
+                if (board.chessboardPosition[i].transform.GetChild(0).name == unit.transform.parent.name)
+                {
+                    Debug.Log("Aşama 3");
+                    if (starCount < 3)
+                    {
+                        Debug.Log("Aşama 4");
+                        tempUpgradeUnit.Add(unit.transform.parent.gameObject);
+                        starCount++;
+                    }
+                }
+            }
+            
+        }
+        
+        for (int i = 0; i < 8; i++)
+        {
+            Debug.Log("Aşama 5");
+            if (board.benchPosition[i].transform.childCount > 0)
+            {
+                Debug.Log("Aşama 6 ");
+                if (board.benchPosition[i].transform.GetChild(0).name == unit.transform.parent.name)
+                {
+                    Debug.Log("Aşama 7");
+                    if (starCount < 3)
+                    {
+                        Debug.Log("Aşama 8");
+                        tempUpgradeUnit.Add(unit.transform.parent.gameObject);
+                        starCount++;
+                    }
+                }
             }
         }
 
+        if(starCount > 2)
+        {
+            Debug.Log("Aşama 9");
+            GameObject a = Instantiate(unit, unit.transform) as GameObject;
+            a.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            for (int i = starCount - 1; i >= 0; i--)
+            {
+                Debug.Log("Aşama 10");
+                tempUpgradeUnit.RemoveAt(i);
+            }
+            starCount = 0;
+             /*unit.transform.parent.GetComponent<Pieces>().star;*/
+        }
+        else
+        {
+            Debug.Log("Aşama 11");
+            
+            for (int i = starCount-1; i >= 0; i--)
+            {
+                Debug.Log("Aşama 12");
+                tempUpgradeUnit.RemoveAt(i);
+            }
+            starCount = 0;
+        }
     }
+
     public void BotBuyUnit(Board board, PlayerPurse purse, GameObject tempSelectedUnit)
     {
 
@@ -160,4 +247,5 @@ public class Shop : MonoBehaviour
         }
 
     }
+    
 }
